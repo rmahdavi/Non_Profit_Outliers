a9a2114f130e07cb4cb0578b9226f578bfae2297
rmahdavi <ramahdavi@gmail.com>
2016-11-18 17:28:35 -0800

    Updated

--- Data_Set.py
+++ Data_Set.py
@@ -1,101 +1,116 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
 from sklearn import svm
 
-#Take form 990 extract from the IRS website in in a space-delimited, ASCII format and insert
-#information into a dataframe 
-#url = 'https://www.irs.gov/pub/irs-soi/15eofinextract990.dat.dat'
-#df_form990extract = pd.read_csv(url, delim_whitespace = True)
-#print "df_form990extract"
-#print "Form 990 Extract Basic information below:" 
-#print df_form990extract.info()
-#print 'Functions available:' + ' ' + 'Feat' + '  '+' Zeroize'+ '  '+'abn_score'
 
-#df_school = df_form990extract[df_form990extract.operateschoolsY70cd == 'Y']
-
-
-sub = ['totrevenue > 1000000', 'totcntrbgfts > 500000']
-#Takes the form 990 dataframe and list of query arguments in string format and queries the dataframe.
-def subset(df,*args):
-	for i in args:
-		df = df.query(i)
-	return df
-
-
-def feat(df):
+#Create a featurized DataFrame from raw form 990 extracted data DataFrame.
+def feat(df,*args):
+	'''
+    INPUT: Form 990 extract DataFrame
+    OUTPUT: Featurized DataFrame
+    '''
 	EIN = df.EIN.values
 	df_fr = pd.DataFrame(data = EIN, index = None, columns = ['EIN'])
-	#Liabilities to Assets a measure of leverage
-	df_fr['field1'] =  df.totliabend/df.totassetsend
-	#Equity Ratio measure of the the portion of the total assets owned.
-	#df_fr['field2'] = (capitalstktrstend+paidinsurplusend+retainedearnend)/totassetsend
-	#Surplus Ratio Revenue minus Expenses divided by Revenue
-	df_fr['field2'] = (df.totrevenue-df.totfuncexpns)/df.totrevenue
+	#Executive Compensation
+	df_fr['field1'] = df.compnsatncurrofcr/df.totassetsend
+	#Leverage
+	df_fr['field2'] = df.totliabend/df.totassetsend
+	#Solvency
+	df_fr['field3'] = (df.totrevenue-df.totfuncexpns+df.deprcatndepletn)/df.totliabend
 	#Deferred Expenses Ratio
-	df_fr['field3'] = (df.prepaidexpnsend + df.othrassetsend)/df.totassetsend
-	#Investment Return Ratio
-	df_fr['field4'] = df.invstmntinc/df.totnetliabastend
+	df_fr['field4'] = (df.prepaidexpnsend + df.othrassetsend)/df.totassetsend	
+	#Deferred Revenue Ratio
+	df_fr['field5'] = df.deferedrevnuend/df.totrevenue
 	#Depreciation Rate as a portion of Fixed Assets before application of depreciation
-	df_fr['field5'] = df.deprcatndepletn/(df.deprcatndepletn+df.lndbldgsequipend)
-	#Deffered Revenue as a portion of Revenue
-	df_fr['field6'] = df.deferedrevnuend/df.totrevenue
-	#Compensation of officers
-	df_fr['field7'] = df.compnsatncurrofcr/df.totassetsend
-	df_fr['totrevenue'] = df.totrevenue
-	df_fr['totcntrbgfts'] = df.totcntrbgfts
-	#df_fr['field2'] =  (df.nonintcashend+df.svngstempinvend)/df.totassetsend
-	#df_fr['field3'] = df.lndbldgsequipend/df.totassetsend
-	#df_fr['field5'] = df.advrtpromo/df.totfuncexpns
-	#df_fr['field6'] = df.compnsatncurrofcr/df.totassetsend
-	#df_fr['field7'] = df.legalfees/df.totfuncexpns
-	#df_fr['field8']= df.totrevenue/df.totfuncexpns
+	df_fr['field6'] = df.deprcatndepletn/(df.deprcatndepletn+df.lndbldgsequipend)
+	#Surplus Margin
+	df_fr['field7'] = (df.totrevenue-df.totfuncexpns)/df.totrevenue
+	#Fundraising Efficiency
+	df_fr['field8'] = df.netincfndrsng/df.lessdirfndrsng
+	#Dummy variables to keep as indentifier for future uses
+	for category in args:
+		df_fr[category] = df[category]
 	return df_fr
 
-#Converts all Nans and Infinities in the feature collums to zero.
-#Note EIN (unique identifier) must be included as the first column in the Dataframe.
-def zeroize(df):
+#Subset on the data on givin paramaters
+def subset(df,*args):
+	'''
+    INPUT: Dataframe
+    OUTPUT: Queries on the DataFrame
+    '''
+	for q in args:
+		df = df.query(q)
+	return df	
+
+#Converts all Nans and Infinities in DataFrame to zero
+def clean(df):
+	'''
+    INPUT: Dataframe
+    OUTPUT: Dataframe
+    '''
 	columns = df.columns
 	for i in columns[1:]:
 		df[i] = df[i].replace([np.inf, -np.inf], np.nan).fillna(0)
 	return df
 
-#Take in a list of EIN Numbers associated with normal foundations and creates a base to model on the behavior of these organizations
-def Novelty_Detection(df,list):	
-	df_novel = pd.DataFrame(data = None, index = list, columns = None)
-	EIN = df.pop('EIN')
-	Columns = df.columns
-	X = df.values
-	df = pd.DataFrame(data = Data, index = EIN, columns = Columns)
-	df_novel = df_novel.join(df)
-	X_novel = df_novel.values
+# Detects outliers from a population of non profit organizations
+def abn_score(df, metrics):	
+	'''
+    INPUT: Dataframe, Metrics to use for outlier detection
+    OUTPUT: Dataframe
+    '''
+	df_1 = pd.DataFrame()
+	for i in metrics:
+		df_1[i] = df[i]
+	X = df_1.values
+	model = IsolationForest(contamination = .01)
+	fit_model = model.fit(X)
+	abnormal_scores = fit_model.decision_function(X)
+	predict = fit_model.predict(X)
+	return abnormal_scores, predict
+
+#Takes features of a small sample of non profit organizations 
+#and detects outliers that behave abnormally from the sample
+def Novelty_Detection(df,df_novel,metrics):
+	
+	'''
+    INPUT: Clean featurized DataFrame, Dataframe of Novel Unique Identifiers, metrics to use for outlier detection
+    OUTPUT: Series of distance from the svm hyperplane, Series of classified outlier vs. non-outlier
+    '''
+	EIN_metrics = ['EIN'] + metrics
+	df_1 = pd.DataFrame()
+	for i in EIN_metrics:
+		df_1[i] = df[i]
+	df_novel = df_novel.merge(df_1, on ='EIN', how = 'left')
+	df_novel_clean = df_novel.drop_duplicates('EIN').dropna().reindex()
+	del df_1['EIN']
+	X = df_1.values
+	del df_novel_clean['EIN']
+	X_novel = df_novel_clean.values
 	model = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)	
-	dist_svm = model.fit(X_novel).decision_function(X)
-	df['dist'] = dist_svm
-	df = df.sort_values('dist')
-	return df
+	fitted_model = model.fit(X_novel)
+	dist_svm = fitted_model.decision_function(X)
+	predict = fitted_model.predict(X)
+	return dist_svm, predict
 
 
-# Insert Featurized DataFrame and computes Abnormality Scores useing Isolation Forests.  
-def abn_score(df):	
-	EIN = df.pop('EIN')
-	X = df.values
-	model = IsolationForest()
-	abnormal_scores = model.fit(X).decision_function(X)
-	df['EIN'] = EIN
-	df['AS'] = abnormal_scores
-	#df_AS = pd.DataFrame(data = [EIN], index = None, columns = ['EIN'])
-	#df_AS['AS'] = abnormal_scores
-	#df_AS = pd.concat([df_AS, df], axis = 1)
-	df = df.sort_values('AS', ascending =True)
-	return df
 
-#plt.hist(series1, 100, range = [0,1.5], log = True)
-#plt.show()
+#Obtains IRS Form 990 Data from the IRS website.  Obtain only organizations above 1,000,000 in total revenue and 500,000 in contributions.  
+#Featurizes the data and applies both a novelty detection model and abnormality detection model.
 def main():
-	pass
-
+	url = 'https://www.irs.gov/pub/irs-soi/15eofinextract990.dat.dat'
+	df_form990extract = pd.read_csv(url, delim_whitespace = True)
+	df_novel = pd.read_csv('Novel_clean.csv')
+	df_novel.columns = ['EIN']
+	metrics = ['field1', 'field2', 'field3', 'field4', 'field5', 'field6', 'field7', 'field8']
+	df = feat(df_form990extract,'totrevenue','totcntrbgfts')
+	df = subset(df,'totrevenue>1000000', 'totcntrbgfts>500000' )
+	df = clean(df)
+	df['AS'], df['outlier_IF'] = abn_score(df,metrics)
+	df['ND'], df['outlier_SVM'] = Novelty_Detection(df,df_novel,metrics)
+	return df
+	
 
 if __name__ == '__main__':
 	main()
